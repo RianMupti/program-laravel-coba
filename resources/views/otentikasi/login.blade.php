9b2352f331137b4c58f7c95d3fcf2def0686ce2f
RianMupti <rianmupti@gmail.com>
2020-10-20 22:06:57 +0700

    menngunakan login username

--- resources/views/otentikasi/login.blade.php
+++ resources/views/otentikasi/login.blade.php
@@ -42,10 +42,10 @@
                 <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
                     @csrf
                   <div class="form-group">
-                    <label for="email">Email</label>
-                    <input id="email" type="email" class="form-control" name="email" tabindex="1" required autofocus>
+                    <label for="username">username</label>
+                    <input id="username" type="text" class="form-control" name="username" tabindex="1"  autofocus>
                     <div class="invalid-feedback">
-                      Please fill in your email
+                      Please fill in your username
                     </div>
                   </div>
 
