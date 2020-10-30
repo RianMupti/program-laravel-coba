1e59c7cd75d5788b88c21c09f5d85d4cfa216c7f
RianMupti <rianmupti@gmail.com>
2020-10-30 20:52:23 +0700

    membuat dummy data menggunakan seed

---
+++ database/migrations/2020_10_30_201859_create_jabatan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJabatanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jabatan', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_jabatan', 125);
+            $table->integer('gaji_pokok')->default(255);
+            $table->integer('tunjangan_jabatan')->default(255);
+            $table->integer('tunjangan_makan_perhari')->default(255);
+            $table->integer('tunjangan_transport_perhari')->default(255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jabatan');
+    }
+}
