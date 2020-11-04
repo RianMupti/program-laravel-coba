58aa4cb43d77755c47c055177a8d207ed1299426
RianMupti <rianmupti@gmail.com>
2020-11-04 20:26:30 +0700

    menghubungkan table dengan has to many "ini judul yang benar"

--- app/Models/Karyawan_Keluarga.php
+++ app/Models/Karyawan_Keluarga.php
@@ -10,5 +10,11 @@ class Karyawan_Keluarga extends Model
     use HasFactory;
 
     protected $table = 'karyawan_keluarga';
-    protected $primaryKey = 'karyawan_id';
+    // protected $primaryKey = 'karyawan_id';
+
+    public function Karyawan()
+    {
+        return $this->belongsTo('\App\Models\Karyawan', 'karyawan_id');
+        // $karyawan->where('nama','budi')->first()
+    }
 }
